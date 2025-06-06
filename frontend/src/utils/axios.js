ed248b99e00561932087ea51eb9cf509bc585e83
NaveenRaj666 <naveenrajs1812@gmail.com>
2025-06-06 14:01:14 +0530

    vercel connection

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:8000/api/v1",
-    // baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
-    baseURL: ` https://wayfarer-1e0i.onrender.com/api/v1`,
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+    // baseURL: ` https://wayfarer-1e0i.onrender.com/api/v1`,
     headers: {
         "Content-Type": "application/json",
     }
